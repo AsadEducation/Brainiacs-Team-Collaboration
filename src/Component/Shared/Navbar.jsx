b5306dccc1751e3a5fd464d329aabb804326a61a
Aiman03-del <ausiaam83@gmail.com>
2025-03-13 20:55:06 +0600

    Smooth slider transitions & autoplay optimization

--- src/Component/Shared/Navbar.jsx
+++ src/Component/Shared/Navbar.jsx
@@ -12,63 +12,47 @@ const Navbar = () => {
         <div>
             {/* Desktop Navbar */}
             <nav className="relative px-4 py-4 flex justify-between items-center">
-                <a className="text-3xl font-bold leading-none">
-                    Brainiacs
-                </a>
-                <div className="lg:hidden">
-                    <button className="navbar-burger flex items-center p-3" onClick={toggleMenu}>
-                        <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <title>Mobile menu</title>
-                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-                        </svg>
-                    </button>
-                </div>
-                <ul className="hidden relative lg:left-24 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <a className="text-sm hover:text-accent">Home</a>
-                    </motion.li>
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-                        </svg>
-                    </motion.li>
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <a className="text-sm hover:text-accent">About Us</a>
-                    </motion.li>
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-                        </svg>
-                    </motion.li>
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <a className="text-sm hover:text-accent">Services</a>
-                    </motion.li>
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-                        </svg>
-                    </motion.li>
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <a className="text-sm hover:text-accent">Pricing</a>
-                    </motion.li>
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-                        </svg>
-                    </motion.li>
-                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                        <a className="text-sm hover:text-accent">Contact</a>
-                    </motion.li>
-                </ul>
-                <motion.a 
-                    className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 text-sm font-bold rounded-xl transition duration-200 bg-secondary hover:bg-accent text-white"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    Log In
-                </motion.a>
-            </nav>
+    <a className="text-3xl font-bold leading-none" >
+        Brainiacs
+    </a>
+    <div className="lg:hidden">
+        <button className="navbar-burger flex items-center p-3" onClick={toggleMenu}>
+            <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Mobile menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+        </button>
+    </div>
+    <ul className="hidden relative left-24 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
+        <li><a className="text-sm hover:text-accent cursor-pointer" >Home</a></li>
+        <li>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
+        </li>
+        <li><a className="text-sm hover:text-accent cursor-pointer" >About Us</a></li>
+        <li>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
+        </li>
+        <li><a className="text-sm hover:text-accent cursor-pointer" >Services</a></li>
+        <li>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
+        </li>
+        <li><a className="text-sm hover:text-accent cursor-pointer" >Pricing</a></li>
+        <li>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
+        </li>
+        <li><a className="text-sm hover:text-accent cursor-pointer" >Contact</a></li>
+    </ul>
+    <a className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 text-sm font-bold rounded-xl transition duration-200 bg-secondary hover:bg-accent text-white" >Log In</a>
+</nav>
+
 
             {/* Mobile Menu */}
             <AnimatePresence>
@@ -81,47 +65,40 @@ const Navbar = () => {
                         transition={{ duration: 0.3 }}
                     >
                         <div className="flex items-center mb-8">
-                            <a className="mr-auto text-3xl font-bold leading-none text-primary">
+                            <a className="mr-auto text-3xl font-bold leading-none text-primary" >
                                 Brainiacs
                             </a>
                             <button className="navbar-close text-4xl text-primary" onClick={toggleMenu}>
                                 x
                             </button>
                         </div>
-                        <motion.ul>
-                            <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.1 }}>
-                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded">Home</a>
-                            </motion.li>
-                            <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.2 }}>
-                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded">About Us</a>
-                            </motion.li>
-                            <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.3 }}>
-                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded">Services</a>
-                            </motion.li>
-                            <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.4 }}>
-                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded">Pricing</a>
-                            </motion.li>
-                            <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }}>
-                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded">Contact</a>
-                            </motion.li>
-                        </motion.ul>
-                        <motion.div className="mt-auto">
-                            <motion.div 
-                                className="pt-6"
-                                initial={{ opacity: 0 }} 
-                                animate={{ opacity: 1 }} 
-                                transition={{ delay: 0.6 }}
-                            >
-                                <a className="block px-4 py-3 mb-3 text-xs text-center font-semibold leading-none bg-secondary hover:bg-accent text-white rounded-xl">
-                                    Log in
-                                </a>
-                            </motion.div>
-                        </motion.div>
+                        <ul>
+                            <li className="mb-1">
+                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded" >Home</a>
+                            </li>
+                            <li className="mb-1">
+                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded" >About Us</a>
+                            </li>
+                            <li className="mb-1">
+                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded" >Services</a>
+                            </li>
+                            <li className="mb-1">
+                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded" >Pricing</a>
+                            </li>
+                            <li className="mb-1">
+                                <a className="block p-4 text-sm font-semibold text-primary hover:bg-blue-50 hover:text-secondary rounded" >Contact</a>
+                            </li>
+                        </ul>
+                        <div className="mt-auto">
+                            <div className="pt-6">
+                                <a className="block px-4 py-3 mb-3 text-xs text-center font-semibold leading-none bg-secondary hover:bg-accent text-white rounded-xl" >Log in</a>
+                            </div>
+                        </div>
                     </motion.nav>
                 )}
             </AnimatePresence>
         </div>
     );
-};
+}
 
 export default Navbar;
