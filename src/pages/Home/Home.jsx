6484e3481df8217a5d4f38fb00aca4065a5ba4ea
Hasibul Hasan <hasibul277127@gmail.com>
2025-03-13 15:52:25 +0600

    theme customization setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Statistics from "./Statistics/Statistics";
 
 
 const Home = () => {
     return (
         <div>
             <h2 className="text-3xl text-red-500">This is Home</h2>
+            <Statistics></Statistics>
         </div>
     );
 };
