0f51caa18bd77607abae82d8199f82564379de4e
Aiman03-del <ausiaam83@gmail.com>
2025-03-14 04:29:42 +0600

    make more responsive login and signup page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -20,38 +20,37 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center px-5 py-5 text-white min-h-screen">
-      <div className="rounded-4xl shadow-2xl w-full overflow-hidden max-w-lg md:max-w-7xl bg-white">
-        <div className="relative top-10 ">
+      <div className="rounded-4xl shadow-2xl w-full overflow-hidden max-w-lg md:max-w-7xl bg-white pb-48 md:pb-0 ">
+        {/* <div className="hidden md:block relative top-0 ">
           <img className="w-32" src={logo} alt="" />
-        </div>
+        </div> */}
         <div className="md:flex flex-row-reverse w-full h-[95vh]">
           {/* Left side */}
           <div className="flex flex-col justify-center items-center md:w-1/3 py-10 px-10 text-center bg-secondary">
-            <h2 className="text-white text-4xl mb-4 font-bold">
-              Welcome Back!
+            <h2 className=" text-white text-2xl md:text-4xl mb-4 font-bold">
+              Hello, Friend!
             </h2>
-            <p className="text-white text-xl mb-8">
-              To keep connected with us please login with your personal info
-            </p>
-            <button className="text-lg text-white border-2 rounded-full py-3 px-14">
+            <p className="text-white text-lg md:text-xl mb-8">
+            Enter your personal details and start journey with us</p>
+            <button className="text-lg text-white border-2 rounded-full py-1 md:py-3 px-10 md:px-14">
               Sign Up
             </button>
           </div>
           {/* right side */}
           <div className="text-black flex flex-col items-center justify-center gap-10 w-full md:w-2/3 py-10 px-10">
             <div>
-              <h1 className="text-4xl text-secondary font-medium text-center mb-6">
+              <h1 className="text-2xl md:text-4xl text-secondary font-medium text-center mb-6">
                 Log in to Brainiacs
               </h1>
               <div className="flex items-center justify-center gap-4 mb-8">
                 <button className="p-2 border border-gray-300 rounded-full cursor-pointer">
-                  <img className="w-10" src={facebook} alt="" />
+                  <img className="w-7 md:w-10" src={facebook} alt="" />
                 </button>
                 <button className="p-2 border border-gray-300 rounded-full cursor-pointer">
-                  <img className="w-10" src={google} alt="" />
+                  <img className="w-7 md:w-10" src={google} alt="" />
                 </button>
                 <button className="p-3 border border-gray-300 rounded-full cursor-pointer">
-                  <img className="w-8" src={linkedin} alt="" />
+                  <img className="w-5 md:w-8" src={linkedin} alt="" />
                 </button>
               </div>
               <p className="text-center">or use your email for registration</p>
@@ -71,7 +70,7 @@ const Login = () => {
                 <input
                   type="email"
                   id="email"
-                  className="w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
+                  className="w-full lg:w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
                   placeholder="Enter email"
                 />
               </div>
@@ -85,7 +84,7 @@ const Login = () => {
                 <input
                   type="password"
                   id="password"
-                  className="w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
+                  className="w-full lg:w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
                   placeholder="Enter password"
                 />
               </div>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -20,38 +20,38 @@ const SignUp = () => {
 
   return (
     <div className="flex items-center justify-center px-5 py-5 text-white min-h-screen">
-      <div className="rounded-4xl shadow-2xl w-full overflow-hidden max-w-lg md:max-w-7xl bg-white">
-        <div className="relative top-10">
+      <div className="rounded-4xl shadow-2xl w-full overflow-hidden max-w-lg md:max-w-7xl bg-white pb-48 md:pb-0 ">
+        {/* <div className="hidden md:block absolute top-10">
           <img className="w-32" src={logo} alt="" />
-        </div>
+        </div> */}
         <div className="md:flex w-full h-[95vh]">
           {/* Left side */}
           <div className="flex flex-col justify-center items-center md:w-1/3 py-10 px-10 text-center bg-secondary">
-            <h2 className="text-white text-4xl mb-4 font-bold">
+            <h2 className="text-white text-2xl md:text-4xl mb-4 font-bold">
               Welcome Back!
             </h2>
-            <p className="text-white text-xl mb-8">
+            <p className="text-white text-lg md:text-xl mb-8">
               To keep connected with us please login with your personal info
             </p>
-            <button className="text-lg text-white border-2 rounded-full py-3 px-14">
+            <button className="text-lg text-white border-2 rounded-full py-1 md:py-3 px-10 md:px-14">
               Sign Up
             </button>
           </div>
           {/* right side */}
           <div className="text-black flex flex-col items-center justify-center gap-10 w-full md:w-2/3 py-10 px-10">
             <div>
-              <h1 className="text-4xl text-secondary font-medium text-center mb-6">
+              <h1 className="text-2xl md:text-4xl text-secondary font-medium text-center mb-6">
                 Create an account
               </h1>
               <div className="flex items-center justify-center gap-4 mb-8">
                 <button className="p-2 border border-gray-300 rounded-full cursor-pointer">
-                  <img className="w-10" src={facebook} alt="" />
+                  <img className="w-7 md:w-10" src={facebook} alt="" />
                 </button>
                 <button className="p-2 border border-gray-300 rounded-full cursor-pointer">
-                  <img className="w-10" src={google} alt="" />
+                  <img className="w-7 md:w-10" src={google} alt="" />
                 </button>
                 <button className="p-3 border border-gray-300 rounded-full cursor-pointer">
-                  <img className="w-8" src={linkedin} alt="" />
+                  <img className="w-5 md:w-8" src={linkedin} alt="" />
                 </button>
               </div>
               <p className="text-center">or use your email for registration</p>
@@ -71,7 +71,7 @@ const SignUp = () => {
                 <input
                   type="text"
                   id="username"
-                  className="w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
+                  className="w-full lg:w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
                   placeholder="Enter username"
                 />
               </div>
@@ -85,7 +85,7 @@ const SignUp = () => {
                 <input
                   type="email"
                   id="email"
-                  className="w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
+                  className="w-full lg:w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
                   placeholder="Enter email"
                 />
               </div>
@@ -99,11 +99,16 @@ const SignUp = () => {
                 <input
                   type="password"
                   id="password"
-                  className="w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
+                  className="w-full lg:w-lg p-5 pl-12 mt-2 bg-gray-200 rounded-lg focus:outline-none"
                   placeholder="Enter password"
                 />
               </div>
-<p>Already have an account, <Link className="text-secondary" to="/login">Log in</Link></p>
+              <p>
+                Already have an account,{" "}
+                <Link className="text-secondary" to="/login">
+                  Log in
+                </Link>
+              </p>
               <button
                 type="submit"
                 className="mx-auto w-2xs text-xl text-white bg-secondary cursor-pointer hover:bg-accent py-4 px-16 mt-5 rounded-full focus:outline-none"
