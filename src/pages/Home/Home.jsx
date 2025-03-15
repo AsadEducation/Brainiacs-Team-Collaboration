2410791e47b192b19ccf03008771d3f794f7d414
AsadEducation <AsadEducation@outlook.com>
2025-03-16 05:07:41 +0600

    added footer in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pricing/Pricing";
 import Statistics from "./Statistics/Statistics";
 import Features from "./Features/Features";
 import FAQ from "./Faq/Faq";
+import Footer from "./Footer/Footer";
 
 const Home = () => {
     return (
@@ -15,6 +16,7 @@ const Home = () => {
             <Review></Review>
             <Pricing />
             <FAQ />
+            <Footer />
         </div>
     );
 };
