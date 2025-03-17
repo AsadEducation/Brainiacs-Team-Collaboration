51122cca44549e97e4c9757d1dd98dccf35b9a0b
sabila111 <sharminsharker2002@gmail.com>
2025-03-17 15:40:42 +0600

    added footer section to MainLayout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -9,7 +9,7 @@ const MainLayout = () => {
       <div className='min-h-[calc(100vh-68px)]'>
         <Outlet />
       </div>
-      {/* <Footer /> */}
+   
       <Footer></Footer>
     </div>
   )
