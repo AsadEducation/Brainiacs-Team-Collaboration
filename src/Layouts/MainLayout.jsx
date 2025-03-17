cf0efc3e163e19e0b6b3091f47d7bc16673e0be4
sabila111 <sharminsharker2002@gmail.com>
2025-03-17 15:38:29 +0600

    added footer section to MainLayout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router"
 import Navbar from "../Component/Shared/Navbar"
+import Footer from "../pages/Home/Footer/Footer"
 
 const MainLayout = () => {
   return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
         <Outlet />
       </div>
       {/* <Footer /> */}
+      <Footer></Footer>
     </div>
   )
 }
