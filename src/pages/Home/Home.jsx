303e13098810ff2cd75bf380f245a4e49428963d
sabila111 <sharminsharker2002@gmail.com>
2025-03-20 01:35:58 +0600

    resolved conflict

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,7 @@ import Review from "./Review/Review";
 import Pricing from "./pricing/Pricing";
 import Statistics from "./Statistics/Statistics";
 import Features from "./Features/Features";
-import Faq from "./Faq/Faq";
-
+import FAQ from "./Faq/Faq";
 
 
 const Home = () => {
@@ -15,7 +14,7 @@ const Home = () => {
             <Statistics></Statistics>
             <Review></Review>
             <Pricing />
-          <Faq></Faq>
+            <FAQ />
            
         </div>
     );
