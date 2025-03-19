9a411a5269676e3b3b62ac2a70cfbe64291dd638
sabila111 <sharminsharker2002@gmail.com>
2025-03-20 01:29:58 +0600

    fixing the faq conflict

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,8 @@ import Review from "./Review/Review";
 import Pricing from "./pricing/Pricing";
 import Statistics from "./Statistics/Statistics";
 import Features from "./Features/Features";
-import FAQ from "./Faq/Faq";
+import Faq from "./Faq/Faq";
+
 
 
 const Home = () => {
@@ -14,7 +15,7 @@ const Home = () => {
             <Statistics></Statistics>
             <Review></Review>
             <Pricing />
-            <FAQ />
+           <Faq></Faq>
            
         </div>
     );
