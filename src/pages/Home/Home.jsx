45875c71b13a4c65fbc6867d252aa33fed0ec9d6
sabila111 <sharminsharker2002@gmail.com>
2025-03-25 00:20:20 +0600

    conflict resolved

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,8 @@ import Review from "./Review/Review";
 import Pricing from "./pricing/Pricing";
 import Statistics from "./Statistics/Statistics";
 import Features from "./Features/Features";
-import Faq from "./FAQ/Faq";
+
+
 
 
 
@@ -15,7 +16,7 @@ const Home = () => {
             <Statistics></Statistics>
             <Review></Review>
             <Pricing />
-           <Faq></Faq>
+         
         </div>
     );
 };
