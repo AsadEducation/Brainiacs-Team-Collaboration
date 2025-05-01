f08bce2df81a9067aa482fbe521976698cd1ef11
Aiman03-del <ausiaam83@gmail.com>
2025-05-01 20:21:23 +0600

    fix log in error

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
   baseURL: "http://localhost:5000",
-  baseURL:
-    window.location.hostname === "http://localhost:5000"
-      ? `${import.meta.env.VITE_API_URL}`
-      : "https://brainiacs-server.onrender.com",
+  // baseURL:
+  //   window.location.hostname === "http://localhost:5000"
+  //     ? `${import.meta.env.VITE_API_URL}`
+  //     : "https://brainiacs-server.onrender.com",
 });
 
 const useAxiosPublic = () => {
